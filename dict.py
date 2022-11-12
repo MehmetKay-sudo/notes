ffe7109bd5f158d72effd558fa6a1f2d0a70d027
mehmetkayprogramming <mehmet.kay.programming@gmail.com>
2022-11-12 12:05:09 +0100

    first entry in dict

--- dict.py
+++ dict.py
@@ -1,1 +1,7 @@
-
+def quotes():
+    quotes_science = {
+        "scientist": "Galileo",
+        "quote": "You cannot teach a man anything, you can only help him find it within himself.",
+        "source": "https://www.goodreads.com/author/quotes/14190.Galileo_Galilei"
+        }
+print(quotes)
