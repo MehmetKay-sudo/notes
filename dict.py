654438195949bafa996e0840651bee137bb89e91
MehmetKay-sudo <mehmet.kay.programming@gmail.com>
2023-06-15 20:36:17 +0200

    Delete dict.py

--- dict.py
+++
@@ -1,7 +0,0 @@
-def quotes():
-    quotes_science = {
-        "scientist": "Galileo",
-        "quote": "You cannot teach a man anything, you can only help him find it within himself.",
-        "source": "https://www.goodreads.com/author/quotes/14190.Galileo_Galilei"
-        }
-print(quotes)
